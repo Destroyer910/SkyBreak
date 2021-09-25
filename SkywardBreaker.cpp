9c91a902854a773ac1936e434c12fc5d230bc443
Daniel Cook <addisoncook08@gmail.com>
2021-09-24 20:36:54 -0400

    Added cURL requests portions

--- SkywardBreaker.cpp
+++ SkywardBreaker.cpp
@@ -32,16 +32,85 @@ int main()
         exit(0);
     }
     std::cout << "Beginning to send requests...";
-
+    system("");
 }
+
+// Dissected cURL request
+/*
+
+curl "https://skyward.scps.k12.fl.us/scripts/wsisa.dll/WService=wsEAplus/skyporthttp.w" -X POST
+-H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0"
+-H "Accept - Language: en - US, en; q = 0.5"
+//--compressed REMOVED due to Windows being absolutely stupid and not updating the cURL package
+-H "Referer: https://skyward.scps.k12.fl.us/scripts/wsisa.dll/WService=wsEAplus/fwemnu01.w"
+-H "Content-Type: application/x-www-form-urlencoded"
+-H "Origin: https://skyward.scps.k12.fl.us"
+-H "Connection: keep-alive"
+-H "Cookie: lhnContact=cac219c2-7713-4644-b5f1-03d32b7996b1-38502-MZFrgvS"
+-H "Sec-Fetch-Dest: empty"
+-H "Sec-Fetch-Mode: cors"
+-H "Sec-Fetch-Site: same-origin"
+-H "DNT: 1"
+-H "Sec-GPC: 1"
+-H "TE: trailers"
+--data-raw      // oh yeah juicy stuff down here
+    "requestAction=eel&
+    method=extrainfo&
+    codeType=tryLogin&
+    codeValue=USERNAME&
+    hCompName=ESC-AW-SKYWEB01& Company Name? Unsure if this should be changed for anonymity
+    hOSName=Windows"%"20NT&
+    login=USERNAME&
+    password=PASSWORD&
+    SecurityMenuID=0&
+    HomePageMenuID=0&
+    nameid=-1&
+    hNavSearchOption=all&
+    hSecCache=0"%"20items"%"20in"%"200"%"20entities&
+    CurrentProgram=skyportlogin.w&
+    CurrentVersion=010182&
+    SuperVersion=012121&
+    PaCVersion=05.21.06.00.08&
+    Browser=Moz&
+    BrowserVersion=92&
+    BrowserPlatform=Win32&
+    TouchDevice=false&
+    noheader=yes&
+    duserid=-1&
+    hIPInfo=10.1.208.55&
+    HomePage=sepadm01.w&
+    loginID=-1&
+    hUseCGIIP=yes&
+    hScrollBarWidth=17&
+    UserSecLevel=5&
+    UserLookupLevel=5&
+    AllowSpecial=false&
+    hAnon=bjlbYpAByijcxUsV&
+    pState=FL&
+    pCountry=US&
+    hDisplayBorder=true&
+    hAlternateColors=true&
+    screenWidth=1920&
+    screenHeight=1080&
+    hforgotLoginPage=fwemnu01&
+    userAgent=Mozilla"%"2F5.0"%"20(Windows"%"20NT"%"2010.0"%"3B"%"20Win64"%"3B"%"20x64"%"3B"%"20rv"%"3A92.0)"%"20Gecko"%"2F20100101"%"20Firefox"%"2F92.0&osName=Windows"%"2010&
+    brwsInfo=Firefox"%"2092&
+    subversion=92&
+    supported=true&
+    pageused=Desktop&
+    recordLimit=30&
+    disableAnimations=yes&
+    hOpenSave=no&
+    hAutoOpenPref=no&
+    hButtonHotKeyIDs=bCancel&
+    hButtonHotKeys=B&
+    hLoadTime=.016&
+    lip=4608313c-a9d4-4c8b-a8dd-3cb95047849e.local&
+    cUserRole=family"%"2Fstudent&fwtimestamp=1632529538408"
+*/
+
+// Un-cleaned curl request:
+// curl "https://skyward.scps.k12.fl.us/scripts/wsisa.dll/WService=wsEAplus/skyporthttp.w" - X POST - H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0" - H "Accept: */*" - H "Accept-Language: en-US,en;q=0.5" --compressed - H "Referer: https://skyward.scps.k12.fl.us/scripts/wsisa.dll/WService=wsEAplus/fwemnu01.w" - H "Content-Type: application/x-www-form-urlencoded" - H "Origin: https://skyward.scps.k12.fl.us" - H "Connection: keep-alive" - H "Cookie: lhnContact=cac219c2-7713-4644-b5f1-03d32b7996b1-38502-MZFrgvS" - H "Sec-Fetch-Dest: empty" - H "Sec-Fetch-Mode: cors" - H "Sec-Fetch-Site: same-origin" - H "DNT: 1" - H "Sec-GPC: 1" - H "TE: trailers" --data - raw "requestAction=eel&method=extrainfo&codeType=tryLogin&codeValue=TACO&hCompName=ESC-AW-SKYWEB01&hOSName=Windows" % "20NT&login=TACO&password=SALAD&SecurityMenuID=0&HomePageMenuID=0&nameid=-1&hNavSearchOption=all&hSecCache=0" % "20items" % "20in" % "200" % "20entities&CurrentProgram=skyportlogin.w&CurrentVersion=010182&SuperVersion=012121&PaCVersion=05.21.06.00.08&Browser=Moz&BrowserVersion=92&BrowserPlatform=Win32&TouchDevice=false&noheader=yes&duserid=-1&hIPInfo=10.1.208.55&HomePage=sepadm01.w&loginID=-1&hUseCGIIP=yes&hScrollBarWidth=17&UserSecLevel=5&UserLookupLevel=5&AllowSpecial=false&hAnon=bjlbYpAByijcxUsV&pState=FL&pCountry=US&hDisplayBorder=true&hAlternateColors=true&screenWidth=1920&screenHeight=1080&hforgotLoginPage=fwemnu01&userAgent=Mozilla" % "2F5.0" % "20(Windows" % "20NT" % "2010.0" % "3B" % "20Win64" % "3B" % "20x64" % "3B" % "20rv" % "3A92.0)" % "20Gecko" % "2F20100101" % "20Firefox" % "2F92.0&osName=Windows" % "2010&brwsInfo=Firefox" % "2092&subversion=92&supported=true&pageused=Desktop&recordLimit=30&disableAnimations=yes&hOpenSave=no&hAutoOpenPref=no&hButtonHotKeyIDs=bCancel&hButtonHotKeys=B&hLoadTime=.016&lip=4608313c-a9d4-4c8b-a8dd-3cb95047849e.local&cUserRole=family" % "2Fstudent&fwtimestamp=1632529538408"
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
